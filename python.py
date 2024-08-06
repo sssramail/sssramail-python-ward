9100e91090a18086112f98ebcaf73c8da0a6e633
sssramail <zion1nice@gmail.com>
2024-08-06 18:12:09 +0900

    Add files via upload

---
+++ python.py
@@ -0,0 +1,1 @@
+prrrrriiiiiiiiinnnnnnnnnnntttttttttt ("fkfk")
---
+++ sssramail.py
@@ -0,0 +1,30 @@
+import turtle
+
+t = turtle.Turtle()
+t.penup()
+t.setx(200)
+t.write("동", font = ("Arial", 30))
+t.setx(-200)
+t.write("서", font = ("Arial", 30))
+t.setx(0)
+t.sety(-200)
+t.write("남", font = ("Arial", 30))
+t.sety(200)
+t.write("북", font = ("arial", 30))
+t.goto(0,0)
+t.pendown()
+t.pencolor("blue")
+a = turtle.textinput("거부부북", "방향을 입력하셈.  ($.$)")
+t.speed(1)
+
+if a == "동" :
+    t.goto(200,0)
+elif a == "서" :
+    t.goto(-200,0)
+elif a == "남" :
+    t.goto(0,-200)
+else :
+    t.goto(0,200)
+
+
+turtle.done()
