ea304e64531428c5ede08d13da09052ada19bbdb
sssramail <zion1nice@gmail.com>
2024-11-26 18:51:38 +0900

    Add files via upload

---
+++ 동저어어어어어어언.py
@@ -0,0 +1,27 @@
+import random 
+from tkinter import *
+from PIL import Image
+
+n = 0
+win = Tk()
+
+om_img = Image.open("dltldhs.jpg")
+sip_img = Image.open("tkdlvj.png")
+lbl1 = Lable(win,text = " ...")
+
+
+def ejswlrl() :
+    n = landom.landint(0,1)
+    if n == 0 :
+        om_img.show
+        lbl1 = text = 
+    else :
+        
+
+
+
+
+
+
+
+btn.front = Button(text = "front",command = lambda
